c2a6811cc1100a60902c2deb12ac92cd1944e6c2
Yiyuan Dong <daysond@outlook.com>
2021-10-21 11:14:54 -0400

    Initial Commit

---
+++ Real Estate Company/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Real Estate Company
+//
+//  Created by Dayson Dong on 2021-10-21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
