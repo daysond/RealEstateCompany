a3bd33c146e4529332bb473d33a1cdee60c4bea4
Yiyuan Dong <daysond@outlook.com>
2021-10-21 11:48:47 -0400

    first commit

---
+++ Real Estate Company/HouseCommon.c
@@ -0,0 +1,8 @@
+//
+//  HouseCommon.c
+//  Real Estate Company
+//
+//  Created by Dayson Dong on 2021-10-21.
+//
+
+#include "HouseCommon.h"
---
+++ Real Estate Company/HouseCommon.h
@@ -0,0 +1,20 @@
+//
+//  HouseCommon.h
+//  Real Estate Company
+//
+//  Created by Dayson Dong on 2021-10-21.
+//
+
+#ifndef HouseCommon_h
+#define HouseCommon_h
+
+#define MAX_STR 255
+#include <stdio.h>
+
+typedef struct{
+    
+    char address[MAX_STR];
+    
+} HouseCommon;
+
+#endif /* HouseCommon_h */
